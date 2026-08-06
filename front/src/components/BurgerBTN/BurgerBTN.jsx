50c1ddd8689a14a8cf24e9103fbcecc745745192
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-08-06 22:08:37 +0300

    Fix Burger button checked

--- front/src/components/BurgerBTN/BurgerBTN.jsx
+++ front/src/components/BurgerBTN/BurgerBTN.jsx
@@ -1,12 +1,12 @@
+// BurgerBTN.jsx
 "use client";
 
 import styles from "./BurgerBTN.module.css";
 
-export default function BurgerBTN() {
+export default function BurgerBTN({ checked, onClick }) {
   return (
     <label className={styles.bar}>
-      <input type="checkbox" />
-
+      <input type="checkbox" checked={checked} onChange={onClick} />
       <span className={`${styles.top} ${styles.line}`}></span>
       <span className={`${styles.middle} ${styles.line}`}></span>
       <span className={`${styles.bottom} ${styles.line}`}></span>
