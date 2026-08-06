58b9c99486f3b99edd09a862068c5f0d106545e4
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-08-06 22:09:31 +0300

    Sidebar: add overlay

--- front/src/components/Sidebar/Sidebar.jsx
+++ front/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,9 @@
 "use client";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 import { useEffect, useState, useRef } from "react";
 import { useSidebarContext } from "@/context/SidebarContext";
@@ -11,31 +16,56 @@ export default function Sidebar({ data }) {
   const [isMounted, setIsMounted] = useState(openSidebar);
 
   const sidebarRef = useRef(null);
-
-  console.log(data);
+  const overlayRef = useRef(null);
 
   useEffect(() => {
     if (openSidebar) {
       setIsMounted(true);
-      setTimeout(() => {
+
+      const timer = setTimeout(() => {
+        const target = overlayRef.current;
+
         sidebarRef.current?.classList.add(styles.open);
+        overlayRef.current?.classList.add(styles.openOverlay);
+
+        if (target) {
+          disableBodyScroll(target);
+        }
       }, 100);
+
+      return () => clearTimeout(timer);
     } else {
+      const target = overlayRef.current;
+
       sidebarRef.current?.classList.remove(styles.open);
+      overlayRef.current?.classList.remove(styles.openOverlay);
+      if (target) enableBodyScroll(target);
+
       const timer = setTimeout(() => {
         setIsMounted(false);
-      }, 500); // час анімації
+      }, 500);
 
       return () => clearTimeout(timer);
     }
   }, [openSidebar]);
 
+  useEffect(() => {
+    return () => {
+      clearAllBodyScrollLocks();
+    };
+  }, []);
+
   if (!isMounted) return null;
 
   return (
-    <aside className={`${styles.sidebar} `} ref={sidebarRef}>
-      <IoClose className={styles.close} onClick={() => setOpenSidebar(false)} />
-      <Form data={data} />
-    </aside>
+    <div className={styles.overlay} ref={overlayRef}>
+      <aside className={`${styles.sidebar} `} ref={sidebarRef}>
+        <IoClose
+          className={styles.close}
+          onClick={() => setOpenSidebar(false)}
+        />
+        <Form data={data} />
+      </aside>
+    </div>
   );
 }
