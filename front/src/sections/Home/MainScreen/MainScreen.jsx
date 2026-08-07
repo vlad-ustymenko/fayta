5df2f6483c37db55432feffbf7e08f1b14e60dc2
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-08-07 23:09:15 +0300

    Home: add social icons on MainScreen

--- front/src/sections/Home/MainScreen/MainScreen.jsx
+++ front/src/sections/Home/MainScreen/MainScreen.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 import Image from "next/image";
+import { AiFillInstagram } from "react-icons/ai";
+import { IoLogoYoutube } from "react-icons/io";
+import { BsFacebook } from "react-icons/bs";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { BsYoutube } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BiLogoInstagramAlt } from "react-icons/bi";
 import styles from "./MainScreen.module.css";
 
 const MainScreen = ({ data }) => {
   return (
     <div className={styles.main}>
-      <Image
-        src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${data.image.url}`}
-        fill
-        sizes="(max-width: 768px) 100vw, (min-width: 768px) and (max-width: 1023px) 100vw, 100vw"
-        alt="main image"
-        style={{ objectFit: "cover" }}
-        className={styles.image}
-      />
+      <div className={styles.imageWrapper}>
+        <Image
+          src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${data.image.url}`}
+          fill
+          // sizes="(max-width: 768px) 100vw, (min-width: 768px) and (max-width: 1023px) 100vw, 100vw"
+          alt="main image"
+          style={{ objectFit: "cover" }}
+          className={styles.image}
+        />
+      </div>
 
       <div className={styles.overlay}></div>
       <div className={styles.content}>
-        <h1 className={styles.title}>Fayta nova</h1>
-        <h1 className={styles.description}>
-          Cучасний житловий квартал у передмісті Ужгорода, створений для тих,
-          хто обирає життя у форматі затишку, спокою та щоденного комфорту.
-        </h1>
+        <h1 className={styles.title}>{data.title}</h1>
+        <h1 className={styles.description}>{data.description}</h1>
+      </div>
+      <div className={styles.socialWrapper}>
+        <a href="#" target="_blank" className={styles.socialLink}>
+          <AiFillInstagram className={styles.icon} />
+        </a>
+        <a href="#" target="_blank" className={styles.socialLink}>
+          <AiFillYoutube className={styles.icon} />
+        </a>
+        <a href="#" target="_blank" className={styles.socialLink}>
+          <BsFacebook className={styles.iconfacebook} />
+        </a>
       </div>
     </div>
   );
