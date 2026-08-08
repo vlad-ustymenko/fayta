433ba83e4089ab3a593e13e9758f40b1c7cdbf23
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2026-08-08 22:54:46 +0300

    LangSwitcher: fix className

--- front/src/components/LangSwitcher/LangSwitcher.jsx
+++ front/src/components/LangSwitcher/LangSwitcher.jsx
@@ -3,12 +3,12 @@ import { useParams } from "next/navigation";
 import styles from "./LangSwitcher.module.css";
 import React from "react";
 
-const LangSwitcher = () => {
+const LangSwitcher = ({ className }) => {
   const { locale } = useParams();
   return (
     <a
       href={locale === "en" ? `/uk` : `/en`}
-      className={styles.langSwitcher}
+      className={`${styles.langSwitcher} ${className}`}
       aria-label={
         locale === "en" ? "Перемкнути на українську" : "Switch to English"
       }
